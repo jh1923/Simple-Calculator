d6c69f722f59866eaf681d855aa6dd5e4ba7f9b9
jh1923 <42978576+jh1923@users.noreply.github.com>
2018-09-17 22:25:11 -0400

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator
+{
+	public int add(int num1, int num2)
+	{
+		int ans_add = num1 + num2;
+		return ans_add;
+	}
+	public int sub(int num_1, int num_2)
+	{
+		int ans_sub = num_1 - num_2;
+		return ans_sub;
+	}
+}
---
+++ src/CalculatorMain.java
@@ -0,0 +1,9 @@
+public class CalculatorMain 
+{
+	public static void main(String[] args) 
+	{
+		Calculator calc = new Calculator();
+		System.out.println(calc.add(137, 283));
+		System.out.print(calc.sub(624, 204));
+	}
+}
